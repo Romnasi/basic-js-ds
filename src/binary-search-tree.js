dd0bc0f84fd4e4c7e931d3e08e17cab1b713d477
Roman Nasyrov <romnasi@gmail.com>
2021-10-18 00:24:52 +0300

    feat: add part of solution

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,26 +1,54 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
-/**
-* Implement simple binary search tree according to task description
-* using Node from extensions
-*/
 module.exports = class BinarySearchTree {
+  constructor (){
+    this._root = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this._root ? this._root : null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    this._root = addWithin(this._root, data);
+
+    function addWithin(node, value) {
+      if(!node) {
+        return new Node(data);
+      }
+
+      if (node.data === data) {
+        return node;
+      }
+
+      if (value < node.value) {
+        node.left = addWithin(node.left, value);
+      } else {
+        node.right = addWithin(node.right, value);
+      }
+
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return searchWithin(this._root, data);
+
+    function searchWithin(node, value) {
+      if (!node && node !== 0) {
+        return false;
+      }
+
+      if (node.data === value) {
+        return true;
+      }
+
+      return value < node.data ? 
+        searchWithin(node.left, value) :
+        searchWithin(node.right, value);
+    }
   }
 
   find(/* data */) {
